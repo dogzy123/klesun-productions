1f368d016f4071506eb9da2d9b568b566781846a
dogzy123 <dogzy123@mail.ru>
2019-10-24 02:51:46 +0300

    build

---
+++ build/precache-manifest.27ffbf1eb8142b487548f7a871672fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74621f15db1996c79e8c59edb8cd298e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ce57106fbd1f9df202",
+    "url": "/static/css/main.d3edbfd7.chunk.css"
+  },
+  {
+    "revision": "77966d110e97011c9eae",
+    "url": "/static/js/2.ea268aad.chunk.js"
+  },
+  {
+    "revision": "c6ce57106fbd1f9df202",
+    "url": "/static/js/main.0daf0978.chunk.js"
+  },
+  {
+    "revision": "daf37efea5ce01eae8db",
+    "url": "/static/js/runtime-main.28f6ede9.js"
+  }
+]);
